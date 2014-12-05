f77b9560ce207a43d65aba91b896430d975a8aae
dennis <dennis@KORTO.local>
2014-12-05 11:55:07 +0100

    Initial Commit

---
+++ elastic/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  elastic
+//
+//  Created by dennis on 05/12/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ elastic/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  elastic
+//
+//  Created by dennis on 05/12/14.
+//  Copyright __MyCompanyName__ 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
